e009cdeb9d6bd7b62664fd1c85c9db46dd8214d4
narakkz <41691425+narakkz@users.noreply.github.com>
2019-04-07 14:50:29 +0700

    login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,14 +2,43 @@
     <div>
         <b-container fluid >
             <b-row>
-                <b-col col-6 style="background-color:#333333; height:1080px;">
-                    
+                <b-col col-6 style="background-color:#18a1ba; height:970px;">
+                    <img src="../assets/logo.png" width="100%" height="100%">
                 </b-col>
                 <b-col col-6>
-                    <b-form>
-                        
-                    </b-form>
+                    <b-row>
+                        <b-col>
+                        </b-col>
+                        <b-col>
+                            <b-form style="padding-top:250px;">
+
+                                <img src="../assets/profile.png" style="height:130px; width:130px; background-color:white; border-radius:100px; margin-bottom:20px;">
+
+                                <b-form-input
+                                    id="email-input"
+                                    type="email"
+                                    style="margin-bottom:20px;"
+                                    required
+                                    placeholder="Username"
+                                ></b-form-input>
+
+                                <b-form-input
+                                    id="password-input"
+                                    type="password"
+                                    style="margin-bottom:50px;"
+                                    required
+                                    placeholder="Password"
+                                ></b-form-input>
+
+                                <b-button type="submit" style="width:200px; background-color:#55f355">Login</b-button>
+
+                            </b-form>
+                        </b-col>
+                        <b-col>
+                        </b-col>
+                    </b-row>
                 </b-col>
+
             </b-row>
         </b-container>
     </div>
--- src/views/dashboard.vue
+++ src/views/dashboard.vue
@@ -2,22 +2,28 @@
     <div>
         <b-container fluid>
             <b-row>
-                <b-col md="2" style="background-color:#333333;">
-                    <div style="height:1080px;">
-                        <img src="../assets/profile.png" style="height:130px; width:130px; background-color:white; border-radius:100; margin-top:50px; margin-bottom:25px;"><br>
-                        <p style="color:white; font-size:25px;">Name</p>
+                <b-col style="margin:0; padding:0;">
 
-                        <b-button style="width:100%; margin-bottom:10px;">ยอดค้างชำระ</b-button>
-                        <b-button style="width:100%; margin-bottom:10px;">เบิกจ่าย</b-button>
-                        <b-button style="width:100%;">ออกจากระบบ</b-button>
-                    </div>
-                </b-col>
-                <b-col md="10">
-                    <div style="background-color:#555555; height:300px; padding-top:20px;">
-                        <h2 style="color:white;">ยอดเงินในบัญชี</h2>
-                        <h1 style="color:white;">100,000 บาท</h1>
+                    <b-col md="2" style="background-color:#333333;">
 
-                    </div>
+                        <div style="height:1080px;">
+                            <img src="../assets/profile.png" style="height:130px; width:130px; background-color:white; border-radius:100px; margin-top:50px; margin-bottom:25px;"><br>
+                            <p style="color:white; font-size:25px;">Name</p>
+
+                            <b-button style="width:100%; margin-bottom:10px;">ยอดค้างชำระ</b-button>
+                            <b-button style="width:100%; margin-bottom:10px;">เบิกจ่าย</b-button>
+                            <b-button style="width:100%;">ออกจากระบบ</b-button>
+                        </div>
+
+                    </b-col>
+
+                    <b-col md="10">
+                        <div style="background-color:#555555; height:300px; padding-top:20px;">
+                            <h2 style="color:white;">ยอดเงินในบัญชี</h2>
+                            <h1 style="color:white;">100,000 บาท</h1>
+                        </div>
+                    </b-col>
+
                 </b-col>
             </b-row>
         </b-container>
