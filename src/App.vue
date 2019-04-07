71ae4477a2efa5ddb88483b55322f4b107b5b0a1
narakkz <41691425+narakkz@users.noreply.github.com>
2019-04-07 14:14:31 +0700

    Updated

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    
+
     <router-view/>
   </div>
 </template>
@@ -23,7 +23,5 @@
     }
   }
 }
-html {
-  height: 100%;
-}
+
 </style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,6 +1,17 @@
 <template>
     <div>
-        
+        <b-container fluid >
+            <b-row>
+                <b-col col-6 style="background-color:#333333; height:1080px;">
+                    
+                </b-col>
+                <b-col col-6>
+                    <b-form>
+                        
+                    </b-form>
+                </b-col>
+            </b-row>
+        </b-container>
     </div>
 </template>
 
