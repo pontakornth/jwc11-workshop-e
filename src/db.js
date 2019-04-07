1609f0869f24fb2cd3b7127b2305e34f560bbf36
pontakornth <37771900+pontakornth@users.noreply.github.com>
2019-04-07 13:22:38 +0700

    Add common function

--- src/db.js
+++ src/db.js
@@ -1,66 +1,73 @@
 const DB = {
-    balance: 10200,
-    id: 415,
-    users: [
-        {
-            id : 37512,
-            name: 'Deodat Lawson',
-            bank_account: '13-441156-44466',
-            rank: 2, //2 เหรัญญิก 1 นร.
-            password: "deletethis"
-        },
-        {
-            id : 37513,
-            name: 'DeJohn',
-            bank_account: '13-441156-44466',
-            rank: 1,
-            password: "deletethis"
-        },
-        {
-            id : 37514,
-            name: 'Detergent',
-            bank_account: '13-441156-44466',
-            rank: 1,
-            password: "deletethis"
-        },
-        {
-            id : 37515,
-            name: 'Werewolf',
-            bank_account: '13-441156-44466',
-            rank: 1,
-            password: "deletethis"
-        },
-        {
-            id : 37516,
-            name: 'Lawson',
-            bank_account: '13-441156-44466',
-            rank: 1,
-            password: "deletethis"
-        },
-    ],
-    histories: [
-        {
-            id: 1289,
-            amount: 30,
-            title: 'ค่าไม้กวาด',
-            issuedUserId: 37513,
-            type: 1 // 1 เงินออก 0 เงินเข้า
-        },
-        {
-            id: 1288,
-            amount: 800,
-            title: 'ชำระค่าห้อง',
-            type: 0,
-            issuedUserId: 37512
-        },
-        {
-            id: 1287,
-            amount: 10,
-            title: 'ค่าไม้ถู',
-            type: 1,
-            issuedUserId: 37516
-        }
-    ]
+  balance: 10200,
+  id: 415,
+  users: [
+    {
+      id: 37512,
+      name: 'Deodat Lawson',
+      bank_account: '13-441156-44466',
+      rank: 2, // 2 เหรัญญิก 1 นร.
+      password: 'deletethis'
+    },
+    {
+      id: 37513,
+      name: 'DeJohn',
+      bank_account: '13-441156-44466',
+      rank: 1,
+      password: 'deletethis'
+    },
+    {
+      id: 37514,
+      name: 'Detergent',
+      bank_account: '13-441156-44466',
+      rank: 1,
+      password: 'deletethis'
+    },
+    {
+      id: 37515,
+      name: 'Werewolf',
+      bank_account: '13-441156-44466',
+      rank: 1,
+      password: 'deletethis'
+    },
+    {
+      id: 37516,
+      name: 'Lawson',
+      bank_account: '13-441156-44466',
+      rank: 1,
+      password: 'deletethis'
+    }
+  ],
+  transactions: [
+    {
+      id: 1289,
+      amount: 30,
+      title: 'ค่าไม้กวาด',
+      issuedUserId: 37513,
+      status: 'CONFIRMED',
+      type: 'WITHDRAW'
+    },
+    {
+      id: 1288,
+      amount: 800,
+      title: 'ชำระค่าห้อง',
+      type: 'DEPOSIT',
+      status: 'CONFIRMED',
+      paidUsers: [
+        37512,
+        37515
+
+      ]
+    },
+    {
+      id: 1287,
+      amount: 10,
+      title: 'ค่าไม้ถู',
+      type: 'WITHDRAW',
+      status: 'CONFIRMED',
+      issuedUserId: 37516
+    }
+  ]
 }
 
 export default DB
--- src/store.js
+++ src/store.js
@@ -10,17 +10,73 @@ export default new Vuex.Store({
     isLogin: false
   },
   mutations: {
-    login(state) {
-      state.isLogin == true
+    login (state) {
+      state.isLogin = true
+    },
+    reduceMoney (state, transaction) {
+      state.DB.balance -= transaction.amount
+    },
+    addMoney (state, { amount }) {
+      state.DB.balance += amount
+    },
+    makeTransaction (state, transaction) {
+      state.DB.transactions.push(transaction)
+    },
+    updateTransaction (state, newTran) {
+      state.DB.transactions = state.DB.transactions.map(oldTran => { return oldTran.id === newTran.id ? newTran : oldTran })
     }
 
   },
   actions: {
-    login({ commit }) {
+    login ({ commit }) {
       commit('login')
+    },
+    /*
+     transaction : {
+       title ชื่อ
+       amount จำนวนเงิน
+       type 'WITHDRAW' | 'DEPOSIT'
+       date วันที่
+       status 'WAITING' ยังไม่อนุมัติ | 'CONFIRMED' อนุมัติอล้ว
+       paidUsers เฉพาะเรียกเก็บ คนจ่ายแล้ว
+       issuedUserID เฉพาะเบิกเงิน คนจขอเบิก
+     }
+   */
+    makeTransaction ({ commit }, detail) {
+      const transaction = {
+        id: Math.floor(Math.random() * 2000),
+        title: detail.title,
+        amount: detail.amount,
+        type: detail.type,
+        status: 'WAITING'
+      }
+      switch (detail.type) {
+        case 'WITHDRAW':
+          transaction.issuedUserID = detail.userId
+          break
+        case 'DEPOSIT':
+          transaction.paidUsers = []
+      }
+      commit('makeTransition', transaction)
+    },
+    pay ({ commit, state }, payload) {
+      /*
+        payload {
+          id รายการจ่ายเงิน
+          userId คนจ่ายเงิน
+        }
+       */
+      const transaction = state.DB.transactions.find(x => { return x.id === payload.id })
+      commit('addMoney', transaction.amount)
+      commit('updateTransaction', { ...transaction, paidUsers: [...transaction.paidUsers, payload.userId] })
     }
   },
-/*  getters: {
-    getAllUsers(state)
-  } */
+  getters: {
+    getAllUsers (state) {
+      return state.DB.users
+    },
+    getAllHistories (state) {
+      return state.DB.histories
+    }
+  }
 })
