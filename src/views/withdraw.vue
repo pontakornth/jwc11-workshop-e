06db64cd1a6e10697ea15e4b33be7ddf6e7b9969
pontakornth <37771900+pontakornth@users.noreply.github.com>
2019-04-07 17:33:04 +0700

    balance

--- src/views/withdraw.vue
+++ src/views/withdraw.vue
@@ -18,7 +18,7 @@
                 <div class="col-10" style="width:100%; padding:0;">
                           <b-col cols="12" style="background-color:#15404E; height:250px; padding-top:50px;">
                                   <p style="color:white; font-size:40px;">ยอดเงินในบัญชี</p>
-                                  <p style="color:white; font-size:60px;">100,000.00 <span style="font-size:30px;">บาท</span></p>
+                                  <p style="color:white; font-size:60px;">{{ balance }} <span style="font-size:30px;">บาท</span></p>
                           </b-col>
 
                           <b-col cols="12" style="background-color:#18a1ba; height:100vh; padding-top:10px;">
@@ -67,6 +67,7 @@
 export default {
   data () {
     return {
+      balance: 100000,
       currentTitle: '',
       amount: 0,
       transactions: [
@@ -104,6 +105,7 @@ export default {
           tran.status = 'อนุมัติแล้ว'
         }
       })
+      this.balance -= tran.amount
     }
   }
 }
