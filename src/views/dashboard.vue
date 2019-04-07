f961c547f3c3ad68552e36f75d9649c4b5f9fb41
narakkz <41691425+narakkz@users.noreply.github.com>
2019-04-07 13:24:17 +0700

    Commit !

--- src/views/dashboard.vue
+++ src/views/dashboard.vue
@@ -1,5 +1,13 @@
 <template>
     <div>
-        
+        <b-container fluid>
+            <b-row>
+                <b-col md="2" style="background-color:black;">
+                    <div style="height:1080px;">
+                        aaa
+                    </div>
+                </b-col>
+            </b-row>
+        </b-container>
     </div>
 </template>
